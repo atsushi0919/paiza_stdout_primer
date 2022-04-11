e0cad04ff6096a044c4f8c865a88937443b44e79
Atsushi Hatakeyama <hatake0919@yahoo.co.jp>
2022-04-11 21:57:37 +0900

    セクション1 投稿用の編集

--- 01-01_newline_digit_step1.py
+++ 01-01_newline_digit_step1.py
@@ -1,22 +1,7 @@
 #　1 行の出力 (paizaランク D 相当)
 #　https://paiza.jp/works/mondai/stdout_primer/stdout_primer__newline_digit_step1
 
-# 解答例1
+# 解答例
 n = 813
 # 出力
-puts(n)
-
-# 解答例2
-n = 813
-# 出力
-puts n
-
-# 解答例3
-n = 813
-# 出力
-p(n)
-
-# 解答例4
-n = 813
-# 出力
-p n
+print(n)
--- 01-05_newline_digit_boss.py
+++ 01-05_newline_digit_boss.py
@@ -4,12 +4,11 @@
 # 解答例1
 # 1 から 1000 までの配列 ary を作成
 # ary = [1, 2, 3, ... 998, 999, 1000]
-ary = (1..1000).to_a
+ary = list(range(1, 1001))
 # 出力
-puts(ary)
+[print(num) for num in ary]
 
 # 解答例2
 # 出力
-1.upto(1000) do |num|
-  puts(num)
-end
+for num in range(1, 1001):
+    print(num)
