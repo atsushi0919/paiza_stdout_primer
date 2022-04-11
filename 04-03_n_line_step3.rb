43cdda42708754455e1cbcc7f400081c833d0bb9
atsushi0919 <hatake0919@yahoo.co.jp>
2022-04-11 17:02:25 +0900

    init

---
+++ 04-03_n_line_step3.rb
@@ -0,0 +1,100 @@
+#　10 行以内の出力 (paizaランク D 相当)
+#　https://paiza.jp/works/mondai/stdout_primer/stdout_primer__n_line_step3
+
+# 入出力例
+INPUT1 = <<~"EOS"
+  1
+EOS
+OUTPUT1 = <<~"EOS"
+  1
+EOS
+
+INPUT2 = <<~"EOS"
+  2
+EOS
+OUTPUT2 = <<~"EOS"
+  1
+  2
+EOS
+
+# 解答例1
+# 入力
+n = gets.to_i
+# 1 から n までの配列 ary を生成
+ary = (1..n).to_a
+# 出力
+puts(ary)
+
+# 解答例2
+# 入力
+n = gets.to_i
+# 1 から n まで 1 増やしながら繰り返す
+1.upto(n) do |num|
+  puts(num)
+end
+
+# 解答例3
+# 入力 (1 ≦ n ≦ 10)
+n = gets.to_i
+# 出力
+puts(1) if n >= 1
+puts(2) if n >= 2
+puts(3) if n >= 3
+puts(4) if n >= 4
+puts(5) if n >= 5
+puts(6) if n >= 6
+puts(7) if n >= 7
+puts(8) if n >= 8
+puts(9) if n >= 9
+puts(10) if n == 10
+
+=begin
+10 行以内の出力 (paizaランク D 相当)
+問題にチャレンジして、ユーザー同士で解答を教え合ったり、コードを公開してみよう！
+
+シェア用URL:
+https://paiza.jp/works/mondai/stdout_primer/stdout_primer__n_line_step3
+問題文のURLをコピーする
+ チャレンジする言語
+
+得意な言語を選択してください
+コードを書いて解いてみる
+ チケット使用済
+問題
+ 下記の問題をプログラミングしてみよう！
+数値 N が入力されます。1 から N までの数値をすべて、改行区切りで出力してください。
+
+入力される値
+N
+
+入力値最終行の末尾に改行が１つ入ります。
+文字列は標準入力から渡されます。 標準入力からの値取得方法はこちらをご確認ください
+期待する出力
+答えの数値を出力してください。
+
+
+1
+2
+...
+N
+
+
+末尾に改行を入れ、余計な文字、空行を含んではいけません。
+条件
+すべてのテストケースにおいて、以下の条件をみたします。
+
+* 1 ≦ N ≦ 10
+
+入力例1
+1
+
+出力例1
+1
+
+入力例2
+2
+
+出力例2
+1
+2
+=end
---
+++ 05-02_specific_split_step2.rb
@@ -0,0 +1,99 @@
+# バーティカルライン区切りで 3 つの文字列を出力 (paizaランク D 相当)
+# https://paiza.jp/works/mondai/stdout_primer/stdout_primer__specific_split_step2
+
+# 入出力例
+INPUT1 = <<~"EOS"
+  paiza
+  kirishima
+  kyoko
+EOS
+OUTPUT1 = <<~"EOS"
+  paiza | kirishima | kyoko
+EOS
+
+INPUT2 = <<~"EOS"
+  tvnmmtuzwn
+  yykehmocjk
+  vwsqozutch
+EOS
+OUTPUT2 = <<~"EOS"
+  tvnmmtuzwn|yykehmocjk|vwsqozutch
+EOS
+
+# 解答例1
+n = 3
+# 入力
+ary = n.times.map { gets.chomp }
+# 出力
+puts(ary.join(" | "))
+
+# 解答例2
+# 入力
+s1 = gets.chomp
+s2 = gets.chomp
+s3 = gets.chomp
+# 出力
+puts("#{s1} | #{s2} | #{s3}")
+
+# 解答例3
+# 入力
+s1 = gets.chomp
+s2 = gets.chomp
+s3 = gets.chomp
+# 出力
+puts(s1.to_s + " | " + s2.to_s + " | " + s3.to_i)
+
+=begin
+バーティカルライン区切りで 3 つの文字列を出力 (paizaランク D 相当)
+問題にチャレンジして、ユーザー同士で解答を教え合ったり、コードを公開してみよう！
+
+シェア用URL:
+https://paiza.jp/works/mondai/stdout_primer/stdout_primer__specific_split_step2
+問題文のURLをコピーする
+ チャレンジする言語
+
+得意な言語を選択してください
+コードを書いて解いてみる
+ チケット使用済
+問題
+ 下記の問題をプログラミングしてみよう！
+3 つの文字列が改行区切りで与えられます。
+これらの文字列をバーティカルライン | 区切りで出力してください。
+
+入力される値
+S1
+S2
+S3
+
+入力値最終行の末尾に改行が１つ入ります。
+文字列は標準入力から渡されます。 標準入力からの値取得方法はこちらをご確認ください
+期待する出力
+答えの数値を出力してください。
+
+
+S1|S2|S3
+
+
+末尾に改行を入れ、余計な文字、空行を含んではいけません。
+条件
+すべてのテストケースにおいて、以下の条件をみたします。
+
+* すべての文字列の長さは 1 以上 10 以下
+* すべての文字列は英小文字からなる
+
+入力例1
+paiza
+kirishima
+kyoko
+
+出力例1
+paiza|kirishima|kyoko
+
+入力例2
+tvnmmtuzwn
+yykehmocjk
+vwsqozutch
+
+出力例2
+tvnmmtuzwn|yykehmocjk|vwsqozutch
+=end
