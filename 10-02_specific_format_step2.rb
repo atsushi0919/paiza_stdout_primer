5ba971d6e93b5b2326682f84d6697cc424b414d8
atsushi0919 <hatake0919@yahoo.co.jp>
2022-05-06 17:04:25 +0900

    セクション10 Ruby 解答例修正

--- 10-02_specific_format_step2.rb
+++ 10-02_specific_format_step2.rb
@@ -19,18 +19,18 @@ EOS
 # 解答例1
 # 入力
 n, a, b = gets.split.map(&:to_i)
-# 出力
-puts (["(#{a}, #{b})"] * n).join(", ")
-
-# 解答例2
-# 入力
-n, a, b = gets.split.map(&:to_i)
-# (A, B) 形式の文字列を生成
+# "(a, b)" 形式の文字列を生成
 s = "(#{a}, #{b})"
 # 要素 s が n 個の配列を生成
 ary = [s] * n
 # 出力
 puts(ary.join(", "))
+
+# 解答例2
+# 入力
+n, a, b = gets.split.map(&:to_i)
+# 出力
+puts (["(#{a}, #{b})"] * n).join(", ")
 
 =begin
 文字列と N 個の整数の出力 (paizaランク C 相当)
