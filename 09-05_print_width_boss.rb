d0d2cd3739929a220a4e3ee39f374cb90c54d125
atsushi0919 <hatake0919@yahoo.co.jp>
2022-05-06 16:42:41 +0900

    セクション9 投稿用の編集

--- 09-05_print_width_boss.rb
+++ 09-05_print_width_boss.rb
@@ -10,9 +10,9 @@ INPUT1 = <<~"EOS"
   813
 EOS
 OUTPUT1 = <<~"EOS"
-  0
-  8
-  81
+  __0
+  __8
+  _81
   813
 EOS
 
@@ -22,7 +22,7 @@ n, m = gets.split.map(&:to_i)
 # 続く n 行入力
 ary = n.times.map { gets.to_i }
 # 出力
-ary.each { |num| puts("%#{m}d" % num) }
+ary.each { |a| puts("%#{m}d" % a) }
 
 # 解答例2
 # 1 行目入力
@@ -30,7 +30,7 @@ n, m = gets.split.map(&:to_i)
 # 続く n 行入力
 ary = n.times.map { gets.to_i }
 # 出力
-ary.each { |num| puts("%*d" % [m, num]) }
+ary.each { |a| puts("%*d" % [m, a]) }
 
 # 解答例3
 # 1 行目入力
@@ -38,7 +38,7 @@ n, m = gets.split.map(&:to_i)
 # 続く n 行入力
 ary = n.times.map { gets.to_i }
 # 出力
-ary.each { |num| puts(sprintf("%*d", m, num)) }
+ary.each { |a| puts(sprintf("%*d", m, a)) }
 
 # 解答例4
 # 1 行目入力
@@ -46,7 +46,7 @@ n, m = gets.split.map(&:to_i)
 # 続く n 行入力
 ary = n.times.map { gets.to_i }
 # 出力
-ary.each { |num| puts(format("%*d", m, num)) }
+ary.each { |a| puts(format("%*d", m, a)) }
 
 # 解答例5
 # 1 行目入力
@@ -54,64 +54,4 @@ n, m = gets.split.map(&:to_i)
 # 続く n 行入力
 ary = n.times.map { gets.to_i }
 # 出力
-ary.each { |num| puts(num.to_s.rjust(m)) }
-
-=begin
-【出力幅を指定して出力】N 個の数値を M けた半角スペース埋めで出力 (paizaランク D 相当)
-問題にチャレンジして、ユーザー同士で解答を教え合ったり、コードを公開してみよう！
-
-シェア用URL:
-https://paiza.jp/works/mondai/stdout_primer/stdout_primer__print_width_boss
-問題文のURLをコピーする
- チャレンジする言語
-
-得意な言語を選択してください
-コードを書いて解いてみる
-問題
- 下記の問題をプログラミングしてみよう！
-自然数 N, M と N 個の自然数 A_1, A_2, ..., A_N が与えられます。それぞれの数値が M けたになるよう数値の前に半角スペースを埋めて、改行区切りで出力してください。
-
-入力される値
-N M
-A_1
-A_2
-A_3
-...
-A_N
-
-入力値最終行の末尾に改行が１つ入ります。
-文字列は標準入力から渡されます。 標準入力からの値取得方法はこちらをご確認ください
-期待する出力
-答えの数値を解答の形式に従った形で出力してください。
-
-
-A_1
-A_2
-A_3
-...
-A_N
-
-
-末尾に改行を入れ、余計な文字、空行を含んではいけません。
-条件
-すべてのテストケースにおいて、以下の条件をみたします。
-
-* N, Mは自然数
-* N は 1 以上 99 以下
-* M は 1 以上 10 以下
-* A_i は 0 以上 999 以下
-* (A_i のけた数の最大値) ≦ M
-
-入力例1
-4 3
-0
-8
-81
-813
-
-出力例1
-0
-8
-81
-813
-=end
+ary.each { |a| puts(a.to_s.rjust(m, " ")) }
